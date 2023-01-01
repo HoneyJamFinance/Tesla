9c8419e7bc79479841eace3a98803f56bd130518
HJ <honeyjamfin@gmail.com>
2023-01-01 05:39:01 -0500

    Review of Todo program(start)

---
+++ To_do_program/Todo_program.py
@@ -0,0 +1,41 @@
+# I'm going to recreate the to_do_program I made on jupyter using pycharm
+# 1. for practice
+# 2. To utilize Module
+# 3. To get familiar and utilize git utility
+
+import functions
+
+todos = functions.get_todos()
+
+while True:
+    user_input = input("Type add, show, edit, complete or exit: ")
+
+    if user_input.startswith("add"):
+        todo = user_input[4:]
+        todos.append(todo+'\n')
+        functions.show_todos(todos)
+
+        functions.write_todos(todos)
+
+    elif user_input.startswith("show"):
+
+        functions.show_todos(todos)
+
+    elif user_input.startswith("edit"):
+        edit_number = int(user_input[5:])
+        new_todo = input(f"How do you want to edit {edit_number}?")
+        todos[edit_number-1] = new_todo
+
+        functions.write_todos(todos)
+
+        print("This is your new todo list")
+
+        functions.show_todos(todos)
+
+    elif user_input.startswith("complete"):
+        complete_number = int(user_input[9])
+        print(f"You have completed {complete_number} - {todos[complete_number-1]}")
+        todos.pop(complete_number-1)
+        print("This is the remaining todo list")
+
+        functions.show_todos(todos)
