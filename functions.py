dd4832ed517ace164de5ee42377dbecfbe810969
HJ <honeyjamfin@gmail.com>
2023-01-01 02:34:58 -0500

    Add time label feature(test)

--- functions.py
+++ functions.py
@@ -2,3 +2,5 @@ def write_todos(todos_arg=todos):
     with open(r'C:\Users\Leon\Desktop\jupyter\Python_Tutorial\todos.txt', 'w') as file:
         file.writelines(todos_arg)
         print("does it work?")
+
+print("The time is below: ")
