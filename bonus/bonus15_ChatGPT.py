4bb4f49d6a46f4fed5f357b500dfb6d811428393
HJ <honeyjamfin@gmail.com>
2023-01-01 08:24:19 -0500

    GUI - zip ChatGPT v1.0

---
+++ bonus/bonus15_ChatGPT.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+import zipfile
+
+# Create the layout for the GUI
+layout = [[sg.Text('Enter the name of the file to zip:'), sg.InputText()],
+          [sg.Text('Enter the name of the zip file:'), sg.InputText()],
+          [sg.Button('Create Zip'), sg.Button('Cancel')]]
+
+# Create the window
+window = sg.Window('Zip File Creator', layout)
+
+# Run the event loop to process events and get the values of the inputs
+while True:
+    event, values = window.read()
+
+    # If the 'Create Zip' button is clicked, create the zip file
+    if event == 'Create Zip':
+        with zipfile.ZipFile(values[1], 'w') as myzip:
+            myzip.write(values[0])
+        sg.popup('Zip file created successfully!')
+        break
+
+    # If the 'Cancel' button is clicked or the window is closed, exit the event loop
+    if event in (None, 'Cancel'):
+        break
+
+# Close the window
+window.close()
